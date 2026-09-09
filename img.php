22c5fcecde7012d1282fb788afe59318c6c9be4e
buzzysound <buzzysound01@gmail.com>
2026-09-09 09:44:14 +0100

    integrate public frontend interface

--- img.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_GET['img'])) die;
-if (!isset($_SESSION[$_GET['img']])) die;
-$url = $_SESSION[$_GET['img']];
-
-header("Content-type: image/jpeg");
-readfile( $url );
